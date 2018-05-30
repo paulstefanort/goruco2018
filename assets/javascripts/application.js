8882a908eac02cd7ab2f6aba04aa8bcafb8e7a03
Abel Martin <abel.martin@gmail.com>
2018-05-30 11:01:04 -0400

    Revert "New suggested design"

---
+++ assets/javascripts/application.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.slick-photos').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    centerMode: true,
+    variableWidth: true,
+    autoplay: true
+  });
+});
