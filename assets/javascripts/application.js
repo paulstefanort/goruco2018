d752c688db049e238590e8c14742cb5d716667ec
Abel Martin <abel.martin@gmail.com>
2018-06-06 03:47:17 -0400

    Resolves #38: Re-enables modals.

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+
+  // This is a bit of a hack, but it moves us forward modal-wise
+  $('[data-toggle="modal"]').click(function(e){
+    target = $(e.currentTarget).data('target');
+    $('#'+target).modal();
+  });
+
+  $('button.js-close').on('click', function(e){
+    target = $(e.currentTarget).data('target');
+    $('#'+target).modal('hide');
+  })
+
   if (window.location.hash) {
     if ($(window.location.hash+'.modal').size() > 0) {
       $(window.location.hash).modal();
